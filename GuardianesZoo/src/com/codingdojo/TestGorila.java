cd1e7759efab8080355c5a57733643ab53b7a6eb
Caro9926 <carolinasaavedra01@gmail.com>
2022-02-22 13:08:01 -0500

    primer commit

---
+++ GuardianesZoo/src/com/codingdojo/TestGorila.java
@@ -0,0 +1,13 @@
+package com.codingdojo;
+
+
+public class TestGorila {
+	public static void main(String[] args) {
+       Gorila g = new Gorila(0);
+       g.lanzarAlgo();
+       g.comerBananas();
+       g.escalar();
+       
+    }
+
+}
